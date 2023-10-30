5435df16d264a69248c7acdacf7fa51fdf117273
IgnacioBerra <ignacio.berra@correo.ucu.edu.uy>
2023-10-30 08:26:18 -0300

    Login

--- BDD/conexion.ts
+++ BDD/conexion.ts
@@ -1,7 +1,5 @@
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://NACHO:nacho@databasedesarrolloweb.oulcdob.mongodb.net/?retryWrites=true&w=majority";
-
 const bcrypt = require('bcrypt');
 
 const client = new MongoClient(uri, {
@@ -14,81 +12,51 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-
         await client.connect();
-
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-
-        // await client.close();
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", error);
     }
 }
-run().catch(console.dir);
 
-async function findUser(user, passwordCheck) {
-    try {
+async function findUser(user:string, passwordCheck:string) {
+    let coincidio = false;
+    const db = client.db("Proyecto");
+    const collection = db.collection("Usuarios");
 
+    const userFind = await collection.findOne({ username: user });
 
-        await client.connect();
-
-        const db = client.db("Proyecto");
-
-        const collection = db.collection("Usuarios");
-
-        const userFind = await collection.findOne({ username: user });
-
-        if (userFind) {
-
-            if (await bcrypt.compare(passwordCheck, userFind.password/*.toString()*/)) {
-                console.log("Coincidió.")
-            }
-
-            else {
-                console.log("No coincide");
-                console.log("Stored Password:", userFind.password);
-            }
+    if (userFind) {
+        if (await bcrypt.compare(passwordCheck, userFind.password)) {
+            console.log("Coincidió.");
+            coincidio = true;
+        } else {
+            console.log("No coincide");
+            console.log("Stored Password:", userFind.password);
+            coincidio = false;
         }
     }
-    catch (error) {
-        console.log("Error al intentar iniciar sesión.", error);
-    }
-    finally {
-        await client.close();
-    }
+
+    return coincidio;
 }
 
-async function addUser(user, password) {
+async function addUser(user:string, password:string) {
     try {
-
-        password = await bcrypt.hash(password, 10)
-
-        await client.connect();
-
+        password = await bcrypt.hash(password, 10);
         const db = client.db("Proyecto");
-
-
         const collection = db.collection("Usuarios");
-
         await collection.insertOne({ username: user, password: password });
-
-
     } catch (error) {
         console.log("Error al agregar el usuario:", error);
-    } finally {
-
-        await client.close();
     }
 }
 
+async function main() {
+    await run();
+}
 
-
-run()
-    .then(() => {
-
-        // return addUser("NACHO", "PRUEBA");
-        return findUser("NACHO", "PRUEBA");
-    })
+main()
     .catch(console.dir);
 
 module.exports = {
