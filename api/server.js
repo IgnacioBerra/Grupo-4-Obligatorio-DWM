d574eb423f8e81c9895bcc84e90a3b99774cfcba
IgnacioBerra <ignacio.berra@correo.ucu.edu.uy>
2023-11-08 08:37:13 -0300

    socket pruebas

---
+++ api/server.js
@@ -0,0 +1,49 @@
+
+
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+mongoose.connect(process.env.DATABASE_URL), { useNewUrlParser: true };
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.error("connected to database"))
+
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:4200',
+}));
+
+const proposalRouter = require('./routes/proposal.js');
+const activityRouter = require('./routes/activities.js');
+const loginRouter = require('./routes/login.js');
+const gamesRouter = require('./routes/games.js');
+
+app.use('/proposal', proposalRouter);
+app.use('/activities', activityRouter);
+app.use('/login', loginRouter);
+app.use('/game', gamesRouter);
+
+app.listen(3000, () => console.log('Server started'));
+
+const io = require('socket.io')(3333,{
+    cors:{
+        origin:["http://localhost:8080"],
+    },
+})
+
+io.on("connection",socket =>{
+    console.log(socket.id)
+    socket.on("send", (message,room) => {
+        if (room === '' ){
+            socket.broadcast.emit('custom-event',message)
+        }else{
+            socket.to(room).emit('custom-event',message)
+        }
+    })
+
+})
+
+
