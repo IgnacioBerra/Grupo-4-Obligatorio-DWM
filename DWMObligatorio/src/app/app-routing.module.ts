03ff8aea31cd23e9420abe8c9510091e3964b7a8
BenjaminRielli <101676520+BenjaminRielli@users.noreply.github.com>
2023-11-08 08:57:47 -0300

    css

--- DWMObligatorio/src/app/app-routing.module.ts
+++ DWMObligatorio/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormAdminComponent } from './form-admin/form-admin.component';
 import { AdminPageComponent } from "./admin-page/admin-page.component";
+import { SalaEsperaComponent } from './sala-espera/sala-espera.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: FormAdminComponent },
   { path: 'indexAdmin', component: AdminPageComponent },
+  { path: 'game', component: SalaEsperaComponent}
 ];
 
 @NgModule({
